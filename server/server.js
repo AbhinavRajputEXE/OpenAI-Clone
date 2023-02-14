cca18bf85bfddc276056e835a03a5777925775f8
Abhinav <abhinavrajputaes@gmail.com>
2023-02-14 23:34:50 +0530

    small change

--- server/server.js
+++ server/server.js
@@ -15,7 +15,14 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow_Origin","*");
+    res.header("Access-Control-Allow_Origin","Origin, X-Requested-With,Content-Type,Accept");
+    next();
+});
+
 app.get('/', async (req, res) => {
+  res.header('Access-Control-Allow-Origin','*');
   res.status(200).send({
     message: 'Hello from Clone!'
   })
